5e8c6ab4ad516c26dc52c8a3c060e6308678f87c
jarvispact <jarvispact@gmail.com>
2022-08-24 20:45:07 +0200

    chore: rename system files

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,7 @@
 import { createRenderSystem } from '../webgl2/render-system';
-import { inputSystem } from './input';
-import { levelSystem } from './level';
-import { startupSystem } from './startup';
+import { inputSystem } from './update-input-system';
+import { levelSystem } from './level-system';
+import { startupSystem } from './startup-system';
 import { createUpdateTransformSystem } from './update-transform-system';
 import { updateUiSystem } from './update-ui-system';
 import { world } from './world';
