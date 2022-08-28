e2e771f655df382e2e97d0f43c9ea6622ce67f09
jarvispact <jarvispact@gmail.com>
2022-08-28 11:34:59 +0200

    feat: created 10 levels

--- src/resources/levels.ts
+++ src/resources/levels.ts
@@ -17,7 +17,6 @@ const level1: Level = [
     [E, G, E],
     [E, S, E],
     [E, S, E],
-    [E, S, E],
     [E, P, E],
 ];
 
@@ -25,24 +24,63 @@ const level2: Level = [
     [E, G, E],
     [E, S, S],
     [E, D, S],
-    [E, S, S],
-    [E, P, E],
+    [E, P, S],
 ];
 
 const level3: Level = [
-    [E, G, E],
-    [S, S, S],
-    [S, D, S],
-    [S, S, S],
-    [E, P, E],
+    [E, D, G],
+    [E, D, S],
+    [E, S, S],
+    [E, P, D],
 ];
 
 const level4: Level = [
+    [D, G, S],
+    [D, D, S],
+    [D, D, S],
+    [D, P, S],
+];
+
+const level5: Level = [
     [S, S, G],
     [S, D, D],
+    [S, D, D],
+    [S, P, D],
+];
+
+const level6: Level = [
+    [D, G, D],
+    [D, S, D],
+    [D, S, D],
+    [D, P, D],
+];
+
+const level7: Level = [
+    [G, D, D],
     [S, S, S],
     [D, D, S],
     [D, P, S],
 ];
 
-export const levels: Level[] = [level1, level2, level3, level4];
+const level8: Level = [
+    [S, S, S],
+    [S, D, G],
+    [S, D, D],
+    [S, P, D],
+];
+
+const level9: Level = [
+    [D, D, D],
+    [G, S, S],
+    [D, D, S],
+    [D, P, S],
+];
+
+const level10: Level = [
+    [S, S, S],
+    [S, D, S],
+    [G, D, S],
+    [D, P, S],
+];
+
+export const levels: Level[] = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10];
