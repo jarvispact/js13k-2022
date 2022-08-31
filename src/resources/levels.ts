f74fc841d711ecb51f5c410a416368c95a76af5d
jarvispact <jarvispact@gmail.com>
2022-08-31 20:32:26 +0200

    feat: different levels

--- src/resources/levels.ts
+++ src/resources/levels.ts
@@ -13,124 +13,194 @@ export const PLAYER_TILE = P;
 export type Tile = typeof E | typeof D | typeof S | typeof G | typeof P;
 export type Level = Tile[][];
 
+const l0: Level = [
+    [S, G, S],
+    [S, S, S],
+    [S, S, S],
+    [S, S, S],
+    [S, P, S],
+];
+
 const l1: Level = [
-    [E, G, E],
-    [E, S, E],
-    [E, S, E],
-    [E, P, E],
-    [E, E, E],
+    [S, G, S],
+    [S, D, S],
+    [S, S, S],
+    [S, S, S],
+    [S, P, S],
 ];
 
 const l2: Level = [
-    [E, G, E],
-    [E, S, S],
-    [E, D, S],
-    [E, P, S],
-    [E, E, E],
+    [G, S, S],
+    [D, D, S],
+    [S, S, S],
+    [S, S, S],
+    [S, P, S],
 ];
 
 const l3: Level = [
-    [E, D, G],
-    [E, D, S],
-    [E, S, S],
-    [E, P, D],
-    [E, E, E],
+    [S, D, G],
+    [S, D, S],
+    [S, S, S],
+    [S, S, S],
+    [S, P, S],
 ];
 
 const l4: Level = [
-    [G, D, E],
-    [S, D, E],
-    [S, S, E],
-    [D, P, E],
-    [E, E, E],
+    [S, S, S],
+    [S, G, S],
+    [D, D, S],
+    [S, S, S],
+    [S, P, S],
 ];
 
 const l5: Level = [
-    [D, G, S],
-    [D, D, S],
-    [D, D, S],
-    [D, P, S],
-    [E, E, E],
+    [S, S, S],
+    [S, S, S],
+    [S, D, G],
+    [S, S, D],
+    [S, P, S],
 ];
 
 const l6: Level = [
-    [S, S, G],
-    [S, D, D],
-    [S, D, D],
-    [S, P, D],
-    [E, E, E],
+    [S, S, S],
+    [G, D, S],
+    [D, D, S],
+    [S, S, S],
+    [S, P, S],
 ];
 
 const l7: Level = [
-    [D, G, D],
-    [D, S, D],
-    [D, S, D],
-    [D, P, D],
-    [E, E, E],
+    [S, S, S],
+    [S, S, S],
+    [S, G, D],
+    [S, D, S],
+    [S, P, S],
 ];
 
 const l8: Level = [
-    [G, D, D],
     [S, S, S],
+    [S, D, S],
+    [G, S, S],
     [D, D, S],
-    [D, P, S],
-    [E, E, E],
+    [S, P, S],
 ];
 
 const l9: Level = [
+    [S, G, S],
+    [S, D, S],
     [S, S, S],
-    [S, D, G],
-    [S, D, D],
-    [S, P, D],
-    [E, E, E],
+    [D, S, D],
+    [S, P, S],
 ];
 
 const l10: Level = [
-    [D, D, D],
-    [G, S, S],
-    [D, D, S],
-    [D, P, S],
-    [E, E, E],
-];
-
-const l11: Level = [
-    [S, S, S],
-    [S, D, S],
-    [G, D, S],
-    [D, P, S],
-    [E, E, E],
-];
-
-const l12: Level = [
-    [D, D, D],
     [S, S, S],
     [S, D, G],
-    [S, D, D],
+    [S, D, S],
+    [S, S, S],
     [S, P, D],
 ];
 
-const l13: Level = [
-    [S, S, S],
-    [G, D, S],
+const l11: Level = [
+    [G, S, D],
     [D, S, S],
-    [D, S, D],
-    [D, P, D],
+    [D, D, S],
+    [S, S, S],
+    [S, P, S],
+];
+
+const l12: Level = [
+    [S, D, G],
+    [S, S, S],
+    [S, D, D],
+    [S, D, S],
+    [S, P, S],
+];
+
+const l13: Level = [
+    [D, D, S],
+    [G, S, S],
+    [D, D, S],
+    [S, D, S],
+    [S, P, S],
 ];
 
 const l14: Level = [
-    [S, G, D],
-    [S, D, D],
+    [D, G, S],
+    [S, D, S],
     [S, S, S],
-    [D, D, S],
-    [D, P, S],
+    [D, S, D],
+    [S, P, S],
 ];
 
 const l15: Level = [
-    [D, D, D],
+    [S, S, S],
+    [S, D, G],
+    [S, S, D],
+    [D, S, S],
+    [S, P, S],
+];
+
+const l16: Level = [
+    [S, D, S],
+    [S, S, S],
+    [S, D, G],
+    [S, D, D],
+    [S, P, S],
+];
+
+const l17: Level = [
     [S, S, S],
     [S, D, S],
     [G, D, S],
-    [D, P, S],
+    [D, D, S],
+    [S, P, S],
 ];
 
-export const levels: Level[] = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15];
+const l18: Level = [
+    [S, S, D],
+    [D, G, D],
+    [S, S, S],
+    [S, D, S],
+    [S, P, S],
+];
+
+const l19: Level = [
+    [S, S, S],
+    [S, D, S],
+    [D, D, S],
+    [G, D, S],
+    [S, P, S],
+];
+
+const l20: Level = [
+    [D, D, S],
+    [S, S, S],
+    [S, D, G],
+    [S, S, D],
+    [S, P, S],
+];
+
+export const levels: Level[] = [
+    l0,
+    l1,
+    l2,
+    l3,
+    l4,
+    l5,
+    l6,
+    l7,
+    l8,
+    l9,
+    l10,
+    l11,
+    l12,
+    l13,
+    l14,
+    l15,
+    l16,
+    l17,
+    l18,
+    l19,
+    l20,
+];
